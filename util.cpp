fa9b4df494d82ffbe4779f969f8d4321d999f69d
ecuadros <ecuadros@spc.org.pe>
2026-03-30 20:38:23 -0500

    feat: initialize project structure with main entry point and utility functions

---
+++ util.cpp
@@ -0,0 +1,42 @@
+#include "util.h"
+
+// 0. Ernesto Cuadros <ecuadros@spc.org.pe>
+int suma(int x, int y) {
+    return x + y;
+}
+
+// 1 Alvarez Cadillo, Michell Adrian <<michell.aac.1106@gmail.com>,
+
+// 2 Bernaola Gayoso César Raúl <bernaolacesar@gmail.com>,
+
+// 3 Castro Chaupis, Armando Cristhian <armandocristhiancastrochaupis@gmail.com>,
+
+// 4 Castro Galindo Carlo André <carlocastrogalindo@gmail.com>,
+
+// 5 Diaz Tapia Adderly Orlando <adderly.diaz.tapia@gmail.com>
+
+// 6 Díaz Vega Greta Solange <gretadiaz01@gmail.com>
+
+// 7 DÍAZ VEGA OLENKA HELENE <olenka.diaz.v@uni.pe>
+
+// 8 Gallo Lugo Carlos enrique <cgallolugo@gmail.com>
+
+// 9 Lopez Flores Royer Amed <roamlofl9@gmail.com>
+
+// 10 Miranda Zarate Jorge Luis <mirandazaratejorge@gmail.com>,
+
+// 11 Oscar Toledo Guerrero <tgoscar@gmail.com>
+
+// 12 Quispe Calloapaza, David Saul <quispedavid987@gmail.com>
+
+// 13 Suarez Maciel Susana Isabel <susana.suarez.maciel@gmail.com>
+
+// 14 Tellez Heredia Jhon <phyjhon@gmail.com>,
+
+// 15 Vilca Aguilar Luis Angel <luisangelvilca2@gmail.com>,
+
+// 16 Vinatea Chávez Camilo Jorge <camo2391@gmail.com>
+
+// 20 Segura Nuñez Alex Ernesto <alex.segura.nunez@gmail.com>
+
+// 21 Selis Vasquez Luis Antonio <luisanonioselisvasquez@gmail.com>
